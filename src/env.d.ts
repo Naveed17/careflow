fba53089b3b2a1605d8b1b14e38c8a670331cd72
Naveed17 <42344659+Naveed17@users.noreply.github.com>
2026-08-13 16:39:43 +0500

    gggg

---
+++ src/env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_LICENSE_API_URL?: string;
+  readonly PUBLIC_PADDLE_CLIENT_TOKEN?: string;
+  readonly PUBLIC_PADDLE_ENV?: string;
+  readonly SITE?: string;
+}
